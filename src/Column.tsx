8ed6c1a41c9c90b7b3ea33bc12a775dae9102bd5
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-12 20:15:01 +0800

    allow hiding and showing columns when dragging and dropping

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,25 +1,44 @@
 import { ColumnContainer, ColumnTitle } from './styles';
 import React, { FC, useRef } from 'react';
+import { useDrop } from 'react-dnd';
 import { AddNewItem } from './AddNewItem';
 import { useAppState } from './state/AppStateContext';
 import { Card } from './Card';
-import { addTask } from './state/actions';
+import { addTask, moveList } from './state/actions';
 import { useItemDrag } from './utils/useItemDrag'
+import { isHidden } from './utils/isHidden'
 
 // Represents one group of tasks.
 // Contains a task list and an option to add a new task.
 export const Column: FC<ColumnProps> = ({ text, id }: ColumnProps) => {
-  const { getTasksByListId, dispatch } = useAppState();
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
   const ref = useRef<HTMLDivElement>(null);
   const { drag } = useItemDrag({ type: "COLUMN", id, text });
-  drag(ref);
+
+  const [_, drop] = useDrop({
+    accept: "COLUMN",
+    hover() {
+      if (!draggedItem) {
+        return;
+      }
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) {
+          return;
+        }
+
+        dispatch(moveList(draggedItem.id, id));
+      }
+    }
+  });
+
+  drag(drop(ref));
 
   return (
     // We need a ref to specify the drag target. We know it will be a div element.
     // We manually provide HTMLDivElement type to useRef call, then provide it
     // as ref prop to ColumnContainer.
-    <ColumnContainer ref={ref}>
+    <ColumnContainer ref={ref} isHidden={isHidden(draggedItem, "COLUMN", id)}>
       <ColumnTitle>{text}</ColumnTitle>
       
       {tasks.map((task) => {
